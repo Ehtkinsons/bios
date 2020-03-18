cbc60c6b910f8ed020aa32d8e4f45d04e2abe0d0
Harvey <harveyjwilliams@aol.co.uk>
2020-03-19 00:53:35 +0100

    Made fully compatible with custom start page.

--- src/js/main.js
+++ src/js/main.js
@@ -3,32 +3,10 @@ const ContentPanel = require('./content_panel.js');
 const Navigation = require('./navigation.js');
 require('../css/app.scss');
 
-const data = {
-  "linkGroups": [
-    {
-      "name": "work",
-      "links": [
-        ['http://canvas.ust.hk/', 'Canvas'],
-        ['http://outlook.com/', 'Outlook'],
-        ['http://o365.ust.hk/', 'HKUST Email'],
-        ['http://mail.ck2ustudio.com/', 'Ck2uStudio Email'],
-        ['https://github.com/tommyku', 'GitHub']
-      ],
-    },
-    {
-      "name": "fun",
-      "links": [
-        ['http://reddit.com/', 'Reddit'],
-        ['http://www.nicovideo.jp', 'NicoNico'],
-        ['https://youtube.com', 'YouTube'],
-        ['http://share.dmhy.org/', 'DMHY'],
-        ['http://www.bilibili.com/', 'bilibili']
-      ]
-    }
-  ]
-};
+const storage = new CustomStartStorage();
 
-$(function() {
-  (new Navigation(data)).bootstrap();
-  (new ContentPanel(data)).bootstrap();
-});
+storage.get()
+  .then(data => {
+    (new Navigation(data)).bootstrap();
+    (new ContentPanel(data)).bootstrap();
+  });
--- src/js/pages/link_page/index.js
+++ src/js/pages/link_page/index.js
@@ -1,15 +1,15 @@
 const Page = require('../../page.js');
 
 class LinkPage extends Page {
-  constructor(name, urls) {
+  constructor(name, links) {
     super();
 
     this.name = name;
 
-    this.collection = urls.map(item=> {
+    this.collection = links.map(link=> {
       return {
         html() {
-          return `<a class='nav' href='${item[0]}'>${item[1]} <span style='color: #555555;'>${item[0]}</span></a>`;
+          return `<a class='nav' href='${link.url}'>${link.name} <span style='color: #555555;'>${link.url}</span></a>`;
         }
       };
     });
